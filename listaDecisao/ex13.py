d18cf21526357efd35bcf0b98c2580085c4f3b96
henriquemunizz <henriquedasilvamuniz03@gmail.com>
2026-03-26 16:27:42 -0300

    ex13,14 resolvidos

--- listaDecisao/ex13.py
+++ listaDecisao/ex13.py
@@ -1,4 +1,28 @@
-#13. Desenvolva um algoritmo que informe se uma data é válida ou não. O algoritmo deverá ler
-#2 números inteiros, que representem o dia e o mês e informar se é um dia do mês válido.
-#Desconsidere os casos de ano bissexto, ou seja, fevereiro têm 28 dias.
+# 13. Desenvolva um algoritmo que informe se uma data é válida ou não. O algoritmo deverá ler
+# 2 números inteiros, que representem o dia e o mês e informar se é um dia do mês válido.
+# Desconsidere os casos de ano bissexto, ou seja, fevereiro têm 28 dias.
 
+
+while True:
+    dia = int(input("Informe o dia: "))
+
+    if dia < 1 or dia > 31:
+        print("Erro: dia inválido.")
+        continue
+        
+    mes = int(input("Informe o mês: "))
+    if mes <= 0 or mes > 12:
+        print("Erro: mês inválido.")
+        continue
+
+    elif mes in [4, 6, 9, 11] and  dia > 30:
+        print("Erro: este mês possui no máximo 30 dias.")
+        continue
+
+    elif mes == 2 and (dia < 1 or dia > 28):
+        print("Erro: fevereiro possui no máximo 28 dias.")
+        continue
+        
+    ano = int(input("Informe o ano: "))
+    print(f"Data válida: {dia}/{mes}/{ano}")
+    break
--- listaDecisao/ex14.py
+++ listaDecisao/ex14.py
@@ -5,3 +5,31 @@
 #c) quando o ano for divisível por 400 ele é bissexto
 #d) as últimas regras prevalecem sobre as primeiras
 #Para exemplificar um pouco essas regras, observe que 1900 não foi bissexto mas 2000 foi.
+
+while True:
+    dia = int(input("Informe o dia: "))
+
+    if dia < 1 or dia > 31:
+        print("Erro: dia inválido.")
+        continue
+        
+    mes = int(input("Informe o mês: "))
+    if mes <= 0 or mes > 12:
+        print("Erro: mês inválido.")
+        continue
+
+    elif mes in [4, 6, 9, 11] and dia > 30:
+        print("Erro: este mês possui no máximo 30 dias.")
+        continue
+        J
+    ano = int(input("Informe o ano: "))
+    if mes == 2:
+        if ((ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0)) and dia > 29:
+            print("Erro: fevereiro em ano bissexto possui no máximo 29 dias.")
+            continue
+        elif dia > 28:
+            print("Erro: fevereiro possui no máximo 28 dias.")
+            continue
+    
+    print(f"Data válida: {dia}/{mes}/{ano}")
+    break
